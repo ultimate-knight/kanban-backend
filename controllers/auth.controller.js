2325f173387da4d0bbbf0ef3d42a65420e101ab9
ultimate-knight <maaz2607@gmail.com>
2026-04-09 15:03:09 +0530

    implemented register and login controllers

---
+++ controllers/auth.controller.js
@@ -0,0 +1,34 @@
+const User=require("../models/user.model")
+const jwt=require("jsonwebtoken")
+const bcrypt=require("bcryptjs")
+
+
+exports.register=async (req,res)=>{
+ const {email,password}=req.body;
+
+ const hashed=await bcrypt.hash(password,10)
+
+ const data=await User.create({email,password:hashed});
+
+ return res.status(200).json(data)
+}
+
+exports.login=async (req,res)=>{
+    const {email,password}=req.body;
+
+    const user=await User.findOne(email);
+
+    if(!user){
+        return res.status(400).json({msg:"user not found"})
+    }
+
+    const isMatch=await bcrypt.compare(user.password,password)
+
+    if(!isMatch){
+        return res.status(403).json({msg:"invalid user credentials"})
+    }
+
+    const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
+
+    return res.status(201).json({token})
+}
